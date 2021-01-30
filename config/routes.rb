7cd58b7efdc0d93cf849461b6e4a615efeea783a
mauro guz <mauroguzbejar@gmail.com>
2021-01-30 16:55:33 +0100

    1232

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
     resources :reservations, only: [:new, :create]
   end
 
-  resources :reservations, only: [:destroy]
+  resources :reservations, only: [:index, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
