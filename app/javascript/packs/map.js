347d139197370c1cd1f9ecf85590c4c764c80aac
Aleksi Rendel <aleksi.rendel@gmail.com>
2021-02-06 13:16:08 +0200

    style map and index searchbar

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -4,7 +4,7 @@ const buildMap = (mapElement) => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
+    style: 'mapbox://styles/galeksi/ckkmwt9nt563s17oiepvsh6d2'
   });
 };
 
